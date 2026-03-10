a6692b6f4d4c885ddee065465b475c35a558ebba
scort <7415450+scorttt@users.noreply.github.com>
2026-03-09 18:11:57 -0700

    fix screen clamping

--- player1.gd
+++ player1.gd
@@ -1,32 +1,32 @@
 extends CharacterBody2D
 
 const SPEED: float = 50.0;
+var paddleSlowRate: float = 0.25;
+
+@onready var paddle = %Paddle;
 
 func _physics_process(delta):
+	var paddleHeight: float = paddle.scale.y / 2;
+	var screenTop: float = paddleHeight;
+	var screenBottom: float = get_viewport_rect().size.y - paddleHeight;
+	
 	if Input.is_action_pressed("p1_move_down"):
 		velocity.y += 1;
 	if Input.is_action_pressed("p1_move_up"):
 		velocity.y -= 1;
 
 	position += velocity * SPEED * delta;
-	
-	var paddleHeight: float = %Paddle.scale.y / 2;
-
-	var screenTop: float = paddleHeight;
-	var screenBottom: float = get_viewport_rect().size.y - paddleHeight;
-	
 	position.y = clamp(position.y, screenTop, screenBottom);
-	if position.y == screenTop:
-		velocity.y = 0;
-	if position.y == screenBottom:
-		velocity.y = 0;
-	
-	var paddleSlowRate: float = 0.25;
+
+	if abs(position.y - screenTop) < 0.5:
+		velocity.y = max(velocity.y, 0);
+	if abs(position.y - screenBottom) < 0.5:
+		velocity.y = min(velocity.y, 0);
+		
 	if velocity.y != 0:
 		if velocity.y > 0:
 			velocity.y -= paddleSlowRate;
 		elif velocity.y < 0:
 			velocity.y += paddleSlowRate;
-
 		if abs(velocity.y) < 0.05:
 			velocity.y = 0;
--- player2.gd
+++ player2.gd
@@ -10,6 +10,9 @@ var paddleSlowRate: float = 0.25;
 
 func _physics_process(delta):
 	var paddleHeight: float = paddle.scale.y / 2;
+	var paddleHalf: float = paddleHeight / 2;
+	var screenTop: float = paddleHeight;
+	var screenBottom: float = get_viewport_rect().size.y - paddleHeight;
 
 	onRightSide = true if ball.global_position.x > get_viewport_rect().size.x / 1.5 else false;
 		
@@ -22,12 +25,12 @@ func _physics_process(delta):
 		if Input.is_action_pressed("p2_move_up"):
 			velocity.y -= 1;
 	else:
-		if ball.global_position.y + (paddleHeight / 2) < paddle.global_position.y:
+		if ball.global_position.y + paddleHalf < paddle.global_position.y:
 			if onRightSide:
 				velocity.y -= 1.0;
 			else:
 				velocity.y -= 0.5;
-		elif ball.global_position.y - (paddleHeight / 2) > paddle.global_position.y:
+		elif ball.global_position.y - paddleHalf > paddle.global_position.y:
 			if onRightSide:
 				velocity.y += 1.0;
 			else:
@@ -36,15 +39,12 @@ func _physics_process(delta):
 			velocity.y = 0;
 
 	position += velocity * SPEED * delta;
-
-	var screenTop: float = paddleHeight;
-	var screenBottom: float = get_viewport_rect().size.y - paddleHeight;
-
 	position.y = clamp(position.y, screenTop, screenBottom);
-	if position.y == screenTop:
-		velocity.y = 0;
-	if position.y == screenBottom:
-		velocity.y = 0;
+
+	if abs(position.y - screenTop) < 0.5:
+		velocity.y = max(velocity.y, 0);
+	if abs(position.y - screenBottom) < 0.5:
+		velocity.y = min(velocity.y, 0);
 
 	if velocity.y != 0:
 		if velocity.y > 0:
